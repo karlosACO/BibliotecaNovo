d456f2736cfcedad3446c7e4e2415630d9729d33
karlosACO <cacooliver437@gmail.com>
2022-01-08 17:07:17 -0300

    Inclusões para verificação de senha

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,46 +8,57 @@ using Microsoft.Extensions.Logging;
 using Biblioteca.Models;
 using Microsoft.AspNetCore.Http;
 
+
 namespace Biblioteca.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            Autenticacao.CheckLogin(this);
-            return View();
-        }
-
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Login(string login, string senha)
-        {
-            if(login != "admin" || senha != "123")
-            {
-                ViewData["Erro"] = "Senha inválida";
-                return View();
-            }
-            else
-            {
-                HttpContext.Session.SetString("user", "admin");
-                return RedirectToAction("Index");
-            }
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+      _logger = logger;
     }
+
+
+    public IActionResult Index()
+    {
+      Autenticacao.CheckLogin(this);
+      ViewData["mensagem"] = "Seja bem vindo ao sistema de controle de empréstimo de livros";
+      return View();
+    }
+
+
+    public IActionResult Login()
+    {
+      return View();
+    }
+
+
+    [HttpPost]
+    public IActionResult Login(string login, string senha)
+    {
+      if(Autenticacao.verificaLoginSenha(login, senha, this))
+      {
+
+
+      return RedirectToAction("Index");
+      }
+       else
+       {
+
+
+        ViewData["Erro"] = "Senha inválida";
+        return View();
+                          
+      }
+    }
+
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+  }
 }
